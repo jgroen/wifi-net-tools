ef5cc8a36314f010a2081993b5ef433fb9bb4ccf
jgroen <josh.groen@gmail.com>
2017-01-24 12:48:15 -0600

    Update run.sh

--- chute/run.sh
+++ chute/run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# You can implement logic here to do periodic network tests and save the
-# results.  You can do that directly from this bash script or call another
-# program that you write (e.g. in Python).
+iperf3 -s -D
+iperf3 -s -p 5202 -D
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+    
 while true; do
-    #
-    iperf3 -s -D
-    iperf3 -s -p 5202 -D
-    iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+    sleep 300
 done
 
 # If execution reaches this point, the chute will stop running.
