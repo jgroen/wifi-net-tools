0df0b4a5dc9651800d5875b00ffe36d32757c578
jgroen <josh.groen@gmail.com>
2017-02-06 13:51:28 -0600

    Update run.sh

--- chute/run.sh
+++ chute/run.sh
@@ -5,6 +5,8 @@ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iperf3 -s -D
 iperf3 -s -p 5202 -D
 
+cd /var/www/html
+wget http://www.panocam3d.com/videos3d360/Paragliding%203D%20360%20-%20panocam3d.com.mp4 -O paraglide.mp4
     
 while true; do
     sleep 300
